4a1daa7bb619ece24b6dcb3d21c4dfa56c22ac39
thuanan7 <thuanan7@gmail.com>
2024-03-30 01:34:52 +0700

    Add project files.

---
+++ CrawlerProject_Web/Services/CrawlerService.cs
@@ -0,0 +1,34 @@
+﻿using CrawlerProject_Web.Models;
+using CrawlerProject_Web.Services.IServices;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+
+namespace CrawlerProject_Web.Services
+{
+    public class CrawlerService : ICrawlerService
+    {
+        private readonly string crawlerApiUrl = "https://localhost:7111/api/crawlerapi";
+        public IHttpClientFactory httpClient { get; set; }
+
+        public CrawlerService(IHttpClientFactory httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<List<Conference>> GetConferencesAsync()
+        {
+            var client = httpClient.CreateClient("CrawlerProject");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri(crawlerApiUrl);
+            message.Method = HttpMethod.Get;
+
+            HttpResponseMessage httpResponseMessage = await client.SendAsync(message);
+            var apiContent = await httpResponseMessage.Content.ReadAsStringAsync();
+            var responseContent = JsonConvert.DeserializeObject<List<Conference>>(apiContent);
+
+            
+            return responseContent;
+        }
+    }
+}
